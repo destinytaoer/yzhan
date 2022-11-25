4390bb7d28f7caf88c5dffb25c880c0348d85a8c
destiny <1848765519@qq.com>
2022-11-25 20:27:09 +0800

    fix: logo 展示问题

--- src/widgets/logo/index.tsx
+++ src/widgets/logo/index.tsx
@@ -9,7 +9,7 @@ interface ILogoProps {
 const Logo: FC<ILogoProps> = ({ className, style }) => {
   return (
     <div className={cs('flex items-center', className)} style={style}>
-      <img src='./vite.svg' alt='Vite logo' />
+      <img src='/vite.svg' alt='Vite logo' />
       {/*<h1 className='text-lg ml-2 text-gray-50'>一盏茶时</h1>*/}
     </div>
   )

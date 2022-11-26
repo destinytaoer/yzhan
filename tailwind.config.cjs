f46433bc19f2cdd997cc727ad67f354f0e8f9b2a
destiny <1848765519@qq.com>
2022-11-26 20:48:35 +0800

    fix(material): 修复弹窗中无法使用 tailwindcss 的问题

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  important: '#root',
+  important: '#app',
   theme: {
     extend: {},
   },

1370673e5a3f6e31eccac650de16d1737eb3911b
destiny <1848765519@qq.com>
2022-12-02 20:08:22 +0800

    feat(stock): 实现创建茶包时, 自动创建空库存数据

--- functions/create-teabag/index.js
+++ functions/create-teabag/index.js
@@ -1,4 +1,5 @@
 const cloudbase = require('@cloudbase/node-sdk')
+const { v4: uuid } = require('uuid')
 
 // 初始化
 const app = cloudbase.init({
@@ -11,14 +12,30 @@ const db = app.database()
 
 exports.main = async function (event) {
   const data = JSON.parse(event.body)
-
   const formula_ids = data.formula.map((item) => item._id)
+  const teabagId = uuid()
 
+  const transaction = await db.startTransaction()
+  const teabagCollection = transaction.collection('teabag')
+  const stockCollection = transaction.collection('stock')
   const now = db.serverDate()
-  return db.collection('teabag').add({
+  const res = await teabagCollection.add({
+    _id: teabagId,
     ...data,
     formula_ids,
     created_at: now,
     updated_at: now,
   })
+
+  await stockCollection.add({
+    _id: `TB-${teabagId}`,
+    type: 'TEABAG',
+    name: data.name,
+    remnant_inventory: 0,
+    batch_list: []
+  })
+
+  await transaction.commit()
+
+  return res
 }

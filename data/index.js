7af3720e61653e26dfc0c441b6e8c06c9865d572
destiny <1848765519@qq.com>
2022-12-04 16:31:44 +0800

    feat(data): init stock data by teabag

--- data/index.js
+++ data/index.js
@@ -1,12 +1,25 @@
 const { readJSON, writeJSON } = require('./utils/file')
 const { getDate, updateOldDate } = require('./utils/date')
 
-const materials = readJSON('./material.json')
+// const materials = readJSON('./material.json')
+// const teabags = readJSON('./teabag.json')
+
+// const newMaterials = updateOldDate(materials)
+// const newTeabags = updateOldDate(teabags)
+
+// writeJSON('./material.json', newMaterials)
+// writeJSON('./teabag.json', newTeabags)
+// console.log(newMaterials)
+
 const teabags = readJSON('./teabag.json')
 
-const newMaterials = updateOldDate(materials)
-const newTeabags = updateOldDate(teabags)
+const stock = teabags.map(teabag => ({
+    _id: `TB-${teabag._id}`,
+    _openid: teabag._openid,
+    type: 'TEABAG',
+    name: teabag.name,
+    remnant_inventory: 0,
+    batch_list: []
+}))
 
-writeJSON('./material.json', newMaterials)
-writeJSON('./teabag.json', newTeabags)
-// console.log(newMaterials)
+writeJSON('./stock.json', stock)
